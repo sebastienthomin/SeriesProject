059f41f101ed0f32977b0900461573ae3b83ef97
sebastienthomin <sebastien.thomin@gmail.com>
2018-06-12 10:05:00 +0200

    Liste Séries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { MessageService } from './message.service';
+import { SerieService } from './serie.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SeriesComponent } from './series/series.component';
+import { EpisodesComponent } from './episodes/episodes.component';
+import { SeasonsComponent } from './seasons/seasons.component';
+import { MessagesComponent } from './messages/messages.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeriesComponent,
+    EpisodesComponent,
+    SeasonsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [SerieService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
